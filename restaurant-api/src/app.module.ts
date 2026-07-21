c8ae6a9103f278a3689403f18c9e37c707e53f3b
imanechadli-02 <imanechadli2001@gmail.com>
2026-07-21 21:28:58 +0100

    ajout de la confi de base de donnee

--- restaurant-api/src/app.module.ts
+++ restaurant-api/src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoriesModule } from './categories/categories.module';
+import { ConfigModule } from '@nestjs/config/dist/config.module';
 
 @Module({
-  imports: [CategoriesModule],
+  imports: [CategoriesModule,  ConfigModule.forRoot({
+      isGlobal: true,
+    })],
   controllers: [AppController],
   providers: [AppService],
 })
